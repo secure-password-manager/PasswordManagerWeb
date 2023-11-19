f13f97c85c13c2c7af7c2910aa9a031ea0d273b2
Angie Yeh <angieyeh24@gmail.com>
2023-11-18 20:59:34 -0800

    Enable CDN usage for zxcvbn and not add the library to the bundle
    zxcvbn is a very large library that's 800KB when uncompressed.
    Loading speeds would be reduced if the library is added to the
    bundle.

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,45 @@
 /// <reference types="vitest" />
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { fileURLToPath, URL } from "node:url";
+import { createHtmlPlugin } from "vite-plugin-html";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  resolve: {
+    alias: {
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
+  },
+  plugins: [
+    react(),
+    createHtmlPlugin({
+      inject: {
+        data: {
+          cdnScript:
+            process.env.NODE_ENV === "production"
+              ? '<script src="https://unpkg.com/zxcvbn@4.4.2/dist/zxcvbn.js"></script>'
+              : "",
+        },
+      },
+    }),
+  ],
   test: {
     coverage: {
-      provider: 'istanbul'
+      provider: "istanbul",
     },
     environment: "jsdom",
-  }
-})
+  },
+  build: {
+    sourcemap: process.env.NODE_ENV === "development",
+    rollupOptions: {
+      external: ["zxcvbn"],
+      output: {
+        format: "umd",
+        globals: {
+          zxcvbn: "zxcvbn",
+        },
+      },
+    },
+  },
+});
