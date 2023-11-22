685877348805578a68fdd0d76790fcb88ee08c0f
Angie Yeh <angieyeh24@gmail.com>
2023-11-21 18:09:16 -0800

    Add password generator text field

---
+++ src/components/PasswordGeneratorField.jsx
@@ -0,0 +1,75 @@
+/* eslint-disable react/prop-types */
+import { useState } from "react";
+import { InputAdornment, IconButton, TextField } from "@mui/material";
+import {
+  ContentCopy,
+  LockReset,
+  Visibility,
+  VisibilityOff,
+} from "@mui/icons-material";
+import SnackBar from './SnackBar'
+import PasswordGenerator from "../lib/passwordGenerator";
+
+
+const PasswordGeneratorField = (props) => {
+  const [showPassword, setShowPassword] = useState(false);
+
+  const { password, passwordError, setPassword } = props;
+
+  const showHidePassword = () => setShowPassword(!showPassword);
+  const copyPassword = () => {
+    if (password) {
+      navigator.clipboard.writeText(password);
+    }
+  }
+  const generatePassword = () => {
+    setPassword(pwg.getRandomPassword());
+  };
+
+  const pwg = new PasswordGenerator({
+    includeUpper: true,
+    includeLower: true,
+    includeSymbol: true,
+    includeNumber: true,
+  });
+
+  return (
+    <>
+      <IconButton aria-label="toggle password copy" onClick={generatePassword}>
+        <LockReset />
+      </IconButton>
+      <TextField
+        label="Password"
+        variant="outlined"
+        value={password}
+        error={passwordError.status}
+        helperText={passwordError.message}
+        type={showPassword ? "text" : "password"}
+        onChange={(event) => setPasswordCb(event.target.value)}
+        InputProps={{
+          endAdornment: (
+            <>
+              <InputAdornment position="end">
+                <IconButton
+                  aria-label="toggle password visibility"
+                  onClick={showHidePassword}
+                  onMouseDown={showHidePassword}>
+                  {showPassword ? <Visibility /> : <VisibilityOff />}
+                </IconButton>
+              </InputAdornment>
+              <InputAdornment position="end">
+                <IconButton
+                  aria-label="toggle password copy"
+                  onClick={copyPassword}
+                  onMouseDown={copyPassword}>
+                  <ContentCopy />
+                </IconButton>
+              </InputAdornment>
+            </>
+          ),
+        }}
+      />
+    </>
+  );
+};
+export default PasswordGeneratorField;
