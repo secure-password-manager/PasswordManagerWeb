6cb2da6e21e239b5aa7eafd8b23529c278e0aab9
Derth Adams <derth@me.com>
2023-11-22 13:00:15 -0800

    Get and decrypt user data when DashboardPage loads

--- src/routers/DashboardPage.jsx
+++ src/routers/DashboardPage.jsx
@@ -1,16 +1,25 @@
-import React from "react";
-import { Outlet, Link, useLocation } from "react-router-dom";
+import React, { useEffect, useState } from "react";
+import { useLocation } from "react-router-dom";
+
 import NavBar from "../components/NavBar";
 import SideBar from "../components/SideBar";
 import { Box, Typography } from "@mui/material";
 import VaultItemTiles from "../components/VaultItemTiles";
-import { collections, items } from "../config/data/splitSampleData";
-import NewItemForm from "../components/NewItemForm";
+import { getUserData } from "../common/ServerAPI";
 
 function DashboardPage() {
   const location = useLocation();
+  const [collections, setCollections] = useState([]);
+  const [items, setItems] = useState({});
+  
   const symmetricKey = location.state.symmetricKey;
-  console.log(`symmetric key from location state: ${symmetricKey}`);
+
+  useEffect(() => {
+      getUserData(symmetricKey).then((data) => {
+          setCollections(data.collections);
+          setItems(data.items);
+      })
+  },[]);
 
   return (
     <>
