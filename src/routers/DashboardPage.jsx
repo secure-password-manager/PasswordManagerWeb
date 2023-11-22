c9644193b3bb28d7791910c89dc8a299dfc7b8d7
Derth Adams <derth@me.com>
2023-11-22 14:30:19 -0800

    Remove changes to LoginForm and DashboardPage (pending global state strategy decision)

--- src/routers/DashboardPage.jsx
+++ src/routers/DashboardPage.jsx
@@ -1,27 +1,14 @@
-import React, { useEffect, useState } from "react";
-import { Outlet, Link, useLocation } from "react-router-dom";
+import React from "react";
+import { Outlet, Link } from "react-router-dom";
 
 import NavBar from "../components/NavBar";
 import SideBar from "../components/SideBar";
 import { Box, Typography } from "@mui/material";
 import VaultItemTiles from "../components/VaultItemTiles";
-import { getUserData } from "../common/ServerAPI";
+import { collections, items } from "../config/data/splitSampleData";
 import NewItemForm from "../components/NewItemForm";
 
 function DashboardPage() {
-  const location = useLocation();
-  const [collections, setCollections] = useState([]);
-  const [items, setItems] = useState({});
-  
-  const symmetricKey = location.state.symmetricKey;
-
-  useEffect(() => {
-      getUserData(symmetricKey).then((data) => {
-          setCollections(data.collections);
-          setItems(data.items);
-      })
-  },[]);
-
   return (
     <>
       <NavBar />
