c6fa6ad4558ac18ab0a3b40297e9c1f8285fe878
Derth Adams <derth@me.com>
2023-11-23 12:33:50 -0800

    Add sample data in split/flat format (collections and items separate)

---
+++ src/config/data/splitSampleData.js
@@ -0,0 +1,125 @@
+export const collections = [
+    {
+        name: "Social Media",
+        uuid: "1b7c8ea1-41e6-4415-8a38-5da9f36e4255"
+    },
+    {
+        name: "News",
+        uuid: "44fc3cb7-efea-42a6-aa31-0bf06b0c9f82",
+    },
+    {
+        name: "Finance",
+        uuid: "8acbe9de-1aef-4a33-9d17-8f2b6a79a349",
+    }
+];
+
+export const items = {
+    "e7ca3b88-5250-41c4-bc8d-817ff0a81576": {
+        data: {
+            name: "Mastodon",
+            url: "https://mstdn.social",
+            username: "bob@example.com",
+            password: "xt7mi7XAMG7Ch7nf",
+        },
+        uuid: "e7ca3b88-5250-41c4-bc8d-817ff0a81576",
+        vault_collection: "1b7c8ea1-41e6-4415-8a38-5da9f36e4255",
+        created_at: "2023-11-22T19:44:11.387882Z",
+        modified_at: "2023-11-22T20:09:31.632502Z"
+    },
+    "e9acb795-4c97-456d-88e0-3ec934e5df76": {
+        data: {
+            name: "Facebook",
+            url: "https://facebook.com",
+            username: "bob@example.com",
+            password: "aTIH7tznClGIWpIn",
+        },
+        uuid: "e9acb795-4c97-456d-88e0-3ec934e5df76",
+        vault_collection: "1b7c8ea1-41e6-4415-8a38-5da9f36e4255",
+        created_at: "2023-11-22T19:44:29.280574Z",
+        modified_at: "2023-11-22T20:10:03.285150Z"
+    },
+    "779dd8e2-3f13-4809-a10e-03d89fc3194e": {
+        data: {
+            name: "Instagram",
+            url: "https://instagram.com",
+            username: "bob@example.com",
+            password: "EhRBdoXhEdah3N1K",
+        },
+        uuid: "779dd8e2-3f13-4809-a10e-03d89fc3194e",
+        vault_collection: "1b7c8ea1-41e6-4415-8a38-5da9f36e4255",
+        created_at: "2023-11-22T19:44:29.280574Z",
+        modified_at: "2023-11-22T20:10:03.285150Z"
+    },
+    "c7a3a041-8c76-44ed-ba6c-0a0a4be67823": {
+        data: {
+            name: "New York Times",
+            url: "https://nytimes.com",
+            username: "bob@example.com",
+            password: "V4TQhBZRnGmVNYenw",
+        },
+        uuid: "c7a3a041-8c76-44ed-ba6c-0a0a4be67823",
+        vault_collection: "44fc3cb7-efea-42a6-aa31-0bf06b0c9f82",
+        created_at: "2023-11-22T19:44:29.280574Z",
+        modified_at: "2023-11-22T20:10:03.285150Z"
+    },
+    "ce2e1413-4ac6-4e29-9e6d-780587f39871": {
+        data: {
+            name: "Washington Post",
+            url: "https://wapo.com",
+            username: "bob@example.com",
+            password: "vtRorjmTiiq9ouUxd",
+        },
+        uuid: "ce2e1413-4ac6-4e29-9e6d-780587f39871",
+        vault_collection: "44fc3cb7-efea-42a6-aa31-0bf06b0c9f82",
+        created_at: "2023-11-22T19:44:29.280574Z",
+        modified_at: "2023-11-22T20:10:03.285150Z"
+    },
+    "a10f1f4a-3b67-4f0c-99e2-299c3b40b2e1": {
+        data: {
+            name: "Wall Street Journal",
+            url: "https://wsj.com",
+            username: "bob@example.com",
+            password: "4s5zpS14CQki8MW9z",
+        },
+        uuid: "a10f1f4a-3b67-4f0c-99e2-299c3b40b2e1",
+        vault_collection: "44fc3cb7-efea-42a6-aa31-0bf06b0c9f82",
+        created_at: "2023-11-22T19:44:29.280574Z",
+        modified_at: "2023-11-22T20:10:03.285150Z"
+    },
+    "1e3c56f3-0be0-4e49-9132-94dab58e99c2": {
+        data: {
+            name: "Vanguard",
+            url: "https://vanguard.com",
+            username: "bob@example.com",
+            password: "qD5YvP9NLADgoTSqb",
+        },
+        uuid: "1e3c56f3-0be0-4e49-9132-94dab58e99c2",
+        vault_collection: "8acbe9de-1aef-4a33-9d17-8f2b6a79a349",
+        created_at: "2023-11-22T19:44:29.280574Z",
+        modified_at: "2023-11-22T20:10:03.285150Z"
+    },
+    "1f7e618f-48e5-4c0d-bd75-d5ef872e0dce": {
+        data: {
+            name: "Coinbase",
+            url: "https://vanguard.com",
+            username: "bob@example.com",
+            password: "zI09pHv7pTtG9YxIt",
+        },
+        uuid: "1f7e618f-48e5-4c0d-bd75-d5ef872e0dce",
+        vault_collection: "8acbe9de-1aef-4a33-9d17-8f2b6a79a349",
+        created_at: "2023-11-22T19:44:29.280574Z",
+        modified_at: "2023-11-22T20:10:03.285150Z"
+    },
+    "b530bdf4-9c73-4f47-8d63-36a3c1d69f82": {
+        data: {
+            name: "Silicon Valley Bank",
+            url: "https://svb.com",
+            username: "bob@example.com",
+            password: "lWYUCJXQwMpZ32Wkc",
+        },
+        uuid: "b530bdf4-9c73-4f47-8d63-36a3c1d69f82",
+        vault_collection: "8acbe9de-1aef-4a33-9d17-8f2b6a79a349",
+        created_at: "2023-11-22T19:44:29.280574Z",
+        modified_at: "2023-11-22T20:10:03.285150Z"
+    }
+}
