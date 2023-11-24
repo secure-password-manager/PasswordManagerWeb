f60136eb4471d310015fa3c88d71d72047199747
Derth Adams <derth@me.com>
2023-11-23 19:43:33 -0800

    Store symmetricKey in global state, get user data on DashboardPage load

--- src/routers/DashboardPage.jsx
+++ src/routers/DashboardPage.jsx
@@ -1,13 +1,24 @@
-import React from "react";
-
+import React, { useEffect, useState } from "react";
 import NavBar from "../components/NavBar";
 import SideBar from "../components/SideBar";
 import { Box, Typography } from "@mui/material";
 import VaultItemTiles from "../components/VaultItemTiles";
 
-import { collections, items } from "../config/data/splitSampleData";
+import { getUserData } from "../common/ServerAPI";
+import { useGlobalStore } from "../common/useGlobalStore";
 
 function DashboardPage() {
+  const [collections, setCollections] = useState([]);
+  const [items, setItems] = useState({});
+  const symmetricKey = useGlobalStore((state) => state.symmetricKey);
+
+  useEffect(() => {
+    getUserData(symmetricKey).then((data) => {
+      setCollections(data.collections);
+      setItems(data.items);
+    });
+  }, [symmetricKey]);
+
   return (
     <>
       <NavBar />
