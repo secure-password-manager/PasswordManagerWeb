97d9da84c484146b5d05b3d5131c48587d840040
Derth Adams <derth@me.com>
2023-11-26 18:11:52 -0800

    Store symmetric key in local storage to persist on page reload

--- src/common/useGlobalStore.jsx
+++ src/common/useGlobalStore.jsx
@@ -1,15 +1,20 @@
 import { create } from "zustand";
+import { persist } from "zustand/middleware";
 
-export const useGlobalStore = create((set) => ({
-  symmetricKey: null,
-  setSymmetricKey: (symmetricKey) =>
-    set((state) => ({
-      ...state,
-      symmetricKey: symmetricKey,
-    })),
-  deleteSymmetricKey: () =>
-    set((state) => ({
-      ...state,
-      symmetricKey: null,
-    })),
-}));
+export const useGlobalStore = create(
+  persist((set) => ({
+    symmetricKey: "",
+    setSymmetricKey: (symmetricKey) => {
+      set((state) => ({
+        ...state,
+        symmetricKey: symmetricKey,
+      }))},
+    deleteSymmetricKey: () =>
+      set((state) => ({
+        ...state,
+        symmetricKey: "",
+      })),
+  }), {
+  name: "key-fortress"
+  })
+);
