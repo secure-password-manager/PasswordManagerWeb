1eca32b27b4b06daf92b378a6210448e879afd17
Derth Adams <derth@me.com>
2023-11-28 19:28:33 -0800

    Add state mutation functions for collections and items

---
+++ test/mutationData.js
@@ -0,0 +1,53 @@
+export const newItemData = {
+  data: {
+    name: "Los Angeles Times",
+    url: "https://latimes.com",
+    username: "bob@example.com",
+    password: "ccY2d4OadYkGtHgOHe",
+  },
+  uuid: "de6e1413-4ac6-4e29-9e6d-780587f39871",
+  vault_collection: "44fc3cb7-efea-42a6-aa31-0bf06b0c9f82",
+  created_at: "2023-11-22T19:44:29.280574Z",
+  modified_at: "2023-11-22T20:10:03.285150Z",
+};
+
+export const updatedItemData = {
+  data: {
+    name: "Mastodon",
+    url: "https://mstdn.social",
+    username: "bob@example.com",
+    password: "Mvkza4enJUK6KbW1oV",
+  },
+  uuid: "e7ca3b88-5250-41c4-bc8d-817ff0a81576",
+  vault_collection: "1b7c8ea1-41e6-4415-8a38-5da9f36e4255",
+  created_at: "2023-11-22T19:44:11.387882Z",
+  modified_at: "2023-11-22T20:09:31.632502Z",
+};
+
+export const existingItemData = {
+  data: {
+    name: "New York Times",
+    url: "https://nytimes.com",
+    username: "bob@example.com",
+    password: "V4TQhBZRnGmVNYenw",
+  },
+  uuid: "c7a3a041-8c76-44ed-ba6c-0a0a4be67823",
+  vault_collection: "44fc3cb7-efea-42a6-aa31-0bf06b0c9f82",
+  created_at: "2023-11-22T19:44:29.280574Z",
+  modified_at: "2023-11-22T20:10:03.285150Z",
+}
+
+export const newCollectionData = {
+  name: "Sports",
+  uuid: "af3f5518-f09e-4c22-82c9-58c3623651a4",
+}
+
+export const updatedCollectionData = {
+  name: "Newspapers",
+  uuid: "44fc3cb7-efea-42a6-aa31-0bf06b0c9f82",
+}
+
+export const existingCollectionData = {
+  name: "Finance",
+  uuid: "8acbe9de-1aef-4a33-9d17-8f2b6a79a349",
+}
