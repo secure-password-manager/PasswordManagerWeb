b0a8dab6a29c798f2ed004a4af18f4c2437ad5d7
Angie Yeh <angieyeh24@gmail.com>
2023-11-30 14:39:55 -0800

    Add logout functionality

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,17 +1,53 @@
-import * as React from "react";
+import React, { useState } from "react";
 import { Tab, Box } from "@mui/material";
 import { useNavigate } from "react-router-dom";
-import { Button } from "@mui/base";
+import { logoutAccount } from "@/common/ServerAPI"
+import { useGlobalStore } from "@/common/useGlobalStore";
+import SnackBar from "./SnackBar";
 
 export default function NavBar() {
   const navigate = useNavigate();
+  const symmetricKey = useGlobalStore((state) => state.symmetricKey);
+  const { deleteSymmetricKey } = useGlobalStore();
+  const [snackBarMessaage, setSnackBarMessage] = useState("")
+  const [openSnackbar, setOpenSnackbar] = useState(false);
+
+  const handleLogout = async () => {
+    try {
+      await logoutAccount();
+      deleteSymmetricKey();
+      localStorage.removeItem("key-fortress");
+      navigate("/login-signup");
+    } catch(err) {
+      setSnackBarMessage("Please sign in and try again.")
+      setOpenSnackbar(true);
+      navigate("/login-signup");
+    }
+  }
+
   return (
     <Box sx={{ width: "100%" }}>
       <Box sx={{ borderBottom: 1, borderColor: "divider" }}>
         <Tab label="Home" onClick={() => navigate("/")} />
-        <Tab label="Dashboard" onClick={() => navigate("/dashboard")} />
-        <Tab label="Log in/Sign Up" onClick={() => navigate("/login-signup")} />
+        {symmetricKey && (
+          <Tab label="Dashboard" onClick={() => navigate("/dashboard")} />
+        )}
+        {symmetricKey ? (
+          <Tab label="Log Out" onClick={handleLogout} />
+        ) : (
+          <Tab
+            label="Log in/Sign Up"
+            onClick={() => navigate("/login-signup")}
+          />
+        )}
       </Box>
+      <SnackBar
+        anchorOrigin={{ vertical: "top", horizontal: "center" }}
+        duration={2000}
+        message={snackBarMessaage}
+        open={openSnackbar}
+        setOpenSnackbar={setOpenSnackbar}
+      />
     </Box>
   );
 }
--- src/config/AppConstant.js
+++ src/config/AppConstant.js
@@ -2,17 +2,17 @@ const API_BASE_URL = import.meta.env.DEV ? "http://localhost:8000/" : "https://a
 const CREATE_ACCOUNT_URL = "signup/";
 const ITEMS_URL = "vault_items/"
 const LOGIN_ACCOUNT_URL = "login/";
-const LOGOUT_ACCOUNT_URL = "logout";
+const LOGOUT_ACCOUNT_URL = "logout/";
 const COLLECTIONS_URL = "vault_collections/";
 const USER_KEY = "user_key/"
 
 
 export {
-    API_BASE_URL,
-    COLLECTIONS_URL,
-    CREATE_ACCOUNT_URL,
-    ITEMS_URL,
-    LOGIN_ACCOUNT_URL,
-    LOGOUT_ACCOUNT_URL,
-    USER_KEY
-}
+  API_BASE_URL,
+  COLLECTIONS_URL,
+  CREATE_ACCOUNT_URL,
+  ITEMS_URL,
+  LOGIN_ACCOUNT_URL,
+  LOGOUT_ACCOUNT_URL,
+  USER_KEY,
+};
