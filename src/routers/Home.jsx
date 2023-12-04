ba5e4585163030e4da0bd9927ef584494dccb0e3
Derth Adams <derth@me.com>
2023-12-04 14:52:23 -0800

    Redirect authenticated user from homepage to Dashboard

--- src/routers/Home.jsx
+++ src/routers/Home.jsx
@@ -1,7 +1,22 @@
 import React, { useEffect } from "react";
+import { useLocation, useNavigate } from "react-router-dom";
 import NavBar from "../components/NavBar";
+import { useGlobalStore } from "@/common/useGlobalStore.jsx";
+
 
 function HomePage() {
+  const location = useLocation();
+  const navigate = useNavigate();
+  const symmetricKey = useGlobalStore((state) => state.symmetricKey);
+
+  useEffect(() => {
+    if(symmetricKey) {
+      if(!location.state || !location.state.from || location.state.from !== "navbar") {
+        navigate("/dashboard");
+      }
+    }
+  }, [symmetricKey]);
+
   return (
     <>
       <NavBar />
