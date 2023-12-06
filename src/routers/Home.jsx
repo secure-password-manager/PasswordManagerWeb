a1dea2cb9c298bcb9288201e4fc5c12b113e677f
vnesslange <67447055+vnesslange@users.noreply.github.com>
2023-12-06 00:12:09 -0500

    Remove security section

--- src/routers/Home.jsx
+++ src/routers/Home.jsx
@@ -2,7 +2,6 @@ import React, { useEffect } from "react";
 import NavBar from "../components/NavBar";
 import HeroImageCard from "../components/HeroImage";
 import KeyFeaturesCard from "../components/KeyFeatures";
-import SecurityStaregyCard from "../components/SecurityStrategy";
 
 function HomePage() {
   return (
@@ -10,7 +9,6 @@ function HomePage() {
       <NavBar />
       <HeroImageCard />
       <KeyFeaturesCard />
-      <SecurityStaregyCard />
     </>
   );
 }
